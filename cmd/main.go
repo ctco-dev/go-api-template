956df5853e47b3d48c92fedd4c481d50a1d5316b
soljarka <soljarka@gmail.com>
2019-02-27 16:12:21 +0200

    New routing (#28)
    * Refactor routing and joke handling.
    
    * Fix error text.

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"context"
-	"github.com/ctco-dev/go-api-template/internal/app"
-	"github.com/ctco-dev/go-api-template/internal/log"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/ctco-dev/go-api-template/internal/app"
+	"github.com/ctco-dev/go-api-template/internal/log"
+
 	"github.com/kelseyhightower/envconfig"
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 	"github.com/sirupsen/logrus"
 )
 
@@ -24,40 +24,16 @@ func main() {
 		log.WithCtx(rootCtx).Panicf("env vars error: '%v'", err)
 	}
 
-	someApp := app.NewApp(env)
-
-	http.HandleFunc(
-		"/",
-		func(w http.ResponseWriter, r *http.Request) {
-
-			reqID := uuid.NewV4().String()[0:8]
-			reqCtx := log.NewContext(rootCtx, logrus.Fields{"reqID": reqID})
-			reqCtx, cancel := context.WithTimeout(reqCtx, time.Second*10)
-			defer cancel()
-
-			resp, err := someApp.DoSomething(reqCtx)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				log.WithCtx(reqCtx).Errorf("%+v\n", err)
-				w.Write([]byte("Cant't get new joke"))
-				return
-			}
-
-			bytes, err := json.Marshal(resp)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte("Can't encode response"))
-				return
-			}
-
-			w.WriteHeader(http.StatusOK)
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(bytes)
-
-		})
+	someApp := app.New(env)
+	addr := fmt.Sprintf(":%d", env.Port)
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqID := uuid.NewV4().String()[0:8]
+		reqCtx := log.NewContext(rootCtx, logrus.Fields{"reqID": reqID})
+		reqCtx, cancel := context.WithTimeout(reqCtx, time.Second*10)
+		defer cancel()
+		someApp.ServeHTTP(w, r.WithContext(reqCtx))
+	})
 
 	log.WithCtx(rootCtx).Infof("Server is running at: http://localhost:%d", env.Port)
-	addr := fmt.Sprintf(":%d", env.Port)
-	log.WithCtx(rootCtx).Fatal(http.ListenAndServe(addr, nil))
-
+	log.WithCtx(rootCtx).Fatal(http.ListenAndServe(addr, handler))
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,52 +1,49 @@
 package app
 
 import (
-	"context"
-	"github.com/ctco-dev/go-api-template/internal/log"
-	"time"
+	"net/http"
+	"path"
+	"strings"
 
 	"github.com/ctco-dev/go-api-template/internal/joke"
 )
 
+//Specification is a container of app config parameters
 type Specification struct {
 	JokeServiceURL string `split_words:"true" required:"true" default:"https://api.chucknorris.io/jokes/random"`
 	Port           int    `split_words:"true" required:"true" default:"3000"`
 }
 
-type (
-	App struct {
-		client joke.Client
-	}
-
-	SomeResult struct {
-		Value string
-		Time  string
-	}
-)
-
-func NewApp(env Specification) App {
-
-	apiClient := joke.NewChuckNorrisAPIClient(env.JokeServiceURL)
-
-	return App{
-		client: apiClient,
-	}
-
+// App implements a sample http service
+type app struct {
+	jokeHandler *jokeHandler
 }
 
-func (a *App) DoSomething(ctx context.Context) (result SomeResult, err error) {
+// New creates a new application
+func New(env Specification) http.Handler {
+	return &app{jokeHandler: &jokeHandler{
+		client: joke.NewChuckNorrisAPIClient(env.JokeServiceURL),
+	}}
+}
 
-	log.WithCtx(ctx).Info("I'm trying to get new joke")
-
-	jokeResp, err := a.client.GetJoke(ctx)
-	if err != nil {
-		return result, err
+func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var head string
+	head, r.URL.Path = ShiftPath(r.URL.Path)
+	if head == "joke" {
+		a.jokeHandler.ServeHTTP(w, r)
+		return
 	}
+	http.Error(w, "Not Found", http.StatusNotFound)
+}
 
-	log.WithCtx(ctx).Info("I'm done")
-
-	return SomeResult{
-		Value: jokeResp.Value,
-		Time:  time.Now().String(),
-	}, nil
+// ShiftPath splits off the first component of p, which will be cleaned of
+// relative components before processing. head will never contain a slash and
+// tail will always be a rooted path without trailing slash.
+func ShiftPath(p string) (head, tail string) {
+	p = path.Clean("/" + p)
+	i := strings.Index(p[1:], "/") + 1
+	if i <= 0 {
+		return p[1:], "/"
+	}
+	return p[1:i], p[i:]
 }
